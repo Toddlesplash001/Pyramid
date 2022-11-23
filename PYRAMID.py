0964ecbdae951c2b11d677d00934fe29ea285003
Toddlesplash001 <118872908+Toddlesplash001@users.noreply.github.com>
2022-11-23 10:42:53 +0530

    Add files via upload

---
+++ PYRAMID.py
@@ -0,0 +1,16 @@
+i = 1
+j = 1
+print ("1\n")
+while(i<=5):
+    while(j<=i):
+        print ( j, end = " ") 
+        j = j+1
+
+    while(j>=1):
+        print ( j, end = " ")
+        j = j-1
+
+
+    print ("\n")
+    i = i+1
+     
